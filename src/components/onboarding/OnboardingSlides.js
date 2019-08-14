e789dd58b2a6d73fcbcee776d3db8b4dd3e4a4e3
POLARIS <37786972+polaris905@users.noreply.github.com>
2019-08-14 10:32:53 -0700

    Update OnboardingSlides.js

--- src/components/onboarding/OnboardingSlides.js
+++ src/components/onboarding/OnboardingSlides.js
@@ -67,7 +67,7 @@ class OnboardingSlides extends Component {
           <Row>
             <div className="col-md-6 col-lg-8 my-3 card align-center slide-content" style={{border: 0, borderRadius: "4px"}}>
               <img className="align-center" style={{ width: "100%", maxWidth: "50%", border: 0 }} src="slide1.png" alt="slide1" />
-              <h5 className="my-3 text-slanted align-center">Checking who is online and play with her/him.</h5>
+              <h5 className="my-3 text-slanted align-center">Play with online players.</h5>
             </div>
           </Row>
         </Slide>
@@ -75,7 +75,7 @@ class OnboardingSlides extends Component {
           <Row>
             <div className="col-md-6 col-lg-8 my-3 card align-center slide-content" style={{border: 0, borderRadius: "4px"}}>
               <img className="align-center" style={{ width: "100%", maxWidth: "50%", border: 0 }} src="slide2.png" alt="slide2" />
-              <h5 className="my-3 text-slanted align-center">Enjoy chating with others to make friends.</h5>
+              <h5 className="my-3 text-slanted align-center">Chat with friends.</h5>
             </div>
           </Row>
         </Slide>
@@ -83,7 +83,7 @@ class OnboardingSlides extends Component {
           <Row>
             <div className="col-md-6 col-lg-8 my-3 card align-center slide-content" style={{border: 0, borderRadius: "4px"}}>
               <img className="align-center" style={{ width: "100%", marginTop: "3%", maxWidth: "60%", border: 0 }} src="slide3.png" alt="slide3" />
-              <h5 className="my-3 text-slanted align-center">Moving your paddle to hit the ball.</h5>
+              <h5 className="my-3 text-slanted align-center">Move paddle to play.</h5>
             </div>
           </Row>
         </Slide>
